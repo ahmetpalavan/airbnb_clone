7bbbc75b99682d312a343428c15dbbd14dcd5b48
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-21 01:06:12 +0300

    Update SmallCard

--- airbnb/components/SmallCard.js
+++ airbnb/components/SmallCard.js
@@ -3,12 +3,17 @@ import Image from 'next/image'
 
 const SmallCard = ({img,location,distance}) => {
     return (
-        <div>
+        <div className='flex items-center m-2 mt-5 cursor-pointer hover:bg-gray-100 hover:scale-105 
+        space-x-4 rounded-xl transition transform duration-200 ease-out'>
             {/* Left */}
             <div className='relative h-16 w-16'>
                 <Image
-                src={img} layout='fill'
+                src={img} layout='fill' className='rounded-lg'
                 />
+            </div>
+            <div>
+                <h2>{location}</h2>
+                <h3 className='text-gray-500'>{distance}</h3>
             </div>
         </div>
     )
