5d6bca9212dc9269bde6c2fe2a07cb06f2c28587
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-21 01:05:39 +0300

    Update index.js

--- airbnb/pages/index.js
+++ airbnb/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-import { Banner, Header, SmallCard } from '../components'
+import { Banner, Header, MediumCard, SmallCard } from '../components'
 
 export default function Home(props) {
   const exploreData = props.exploreData
+  const cardsData = props.cardsData
   return (
     <div className=''>
       <Head>
@@ -18,6 +19,7 @@ export default function Home(props) {
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
           {/* Pull some data from a server - API endpoints */}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {exploreData.map(({img,distance,location})=>(
             <SmallCard 
             key={img}
@@ -25,6 +27,16 @@ export default function Home(props) {
             distance={distance} 
             location={location}/>
           ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData.map((a)=>(
+              <MediumCard key={a.img} img={a.img} title={a.title}/>
+            ))}
+          </div>
         </section>
       </main>
     </div>
@@ -45,9 +57,9 @@ import path from 'path'
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data.json');
   const jsonData = await fsPromises.readFile(filePath);
-  const exploreData = JSON.parse(jsonData);
-
+  const ahmet = JSON.parse(jsonData);
   return {
-    props: exploreData
+    props: ahmet
   }
 }
+
