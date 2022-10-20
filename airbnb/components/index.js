40d20b79b12834eacfa3cb0ffa51560c14521a72
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-21 01:07:13 +0300

    Update index.js

--- airbnb/components/index.js
+++ airbnb/components/index.js
@@ -1,3 +1,4 @@
 export {default as Header} from './Header'
 export {default as Banner} from './Banner'
 export {default as SmallCard} from './SmallCard'
+export {default as MediumCard} from './MediumCard'
