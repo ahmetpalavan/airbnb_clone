6324ae5d78a7634382543237cd10490f96633383
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-22 02:58:06 +0300

    React-Date-Range added to Header

--- airbnb/components/Header.js
+++ airbnb/components/Header.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import {MagnifyingGlassIcon} from '@heroicons/react/20/solid'
 import {UserCircleIcon, Bars3Icon, GlobeAltIcon} from '@heroicons/react/24/outline'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
     const [searchInput, setSearchInput] = useState('')
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key:'Selection'
+    }
     return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 '>
         {/* Left */}
@@ -16,7 +27,12 @@ function Header() {
 
         {/* Middle - Search */}
         <div className='items-center flex md:border-2 py-2 rounded-full md:shadow-sm'>
-            <input className='pl-6 bg-transparent outline-none flex-grow text-sm placeholder-gray-400 text-gray-600' type='text' placeholder='Start your search'/>
+            <input
+            value={searchInput}
+            onChange={(e)=>setSearchInput(e.target.value)}
+            className='pl-6 bg-transparent outline-none flex-grow text-sm placeholder-gray-400 text-gray-600' 
+            type='text' 
+            placeholder='Start your search'/>
             <MagnifyingGlassIcon className='h-8 md:mx-2 hidden md:inline-flex bg-red-400 text-white rounded-full cursor-pointer hover:bg-green-300 p-2'/>
         </div>
 
@@ -29,6 +45,15 @@ function Header() {
                 <UserCircleIcon className='h-6'/>
             </div>
         </div>
+        {searchInput && (
+            <div>
+                <DateRangePicker
+                ranges={[selectionRange]}
+                minDate={new Date()}
+                rangeColors={['#FD5B61']}
+                />
+            </div>
+        )}
     </header>
 )
 }
